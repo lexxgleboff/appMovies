b055c9616449280873b08f74112ec6cc191cdc28
lexxgleboff <64305994+lexxgleboff@users.noreply.github.com>
2022-02-21 17:40:33 +0300

    Отредактировал ссылку на репозиторий

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,6 @@ function Footer() {
             <div className="footer-copyright cyan">
             <div className="container">
             © {new Date().getFullYear()} Copyright Alex Glebov
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
             </div>
             </div>
         </footer>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -7,9 +7,7 @@ function Header() {
                 <div className="nav-wrapper">
                     <a href="#" className="brand-logo">Cinema</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Link</a></li>
-                        <li><a href="badges.html">Link</a></li>
-                        <li><a href="collapsible.html">Link</a></li>
+                        <li><a href="https://github.com/lexxgleboff/appmovies">Repository</a></li>
                     </ul>
                 </div>
            </div>
